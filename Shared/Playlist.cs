6b4c6df4358cf35ea65b184e471d448d5972e29c
Ryan Deisler <ryandeisler@mail.adelphi.edu>
2024-10-28 22:33:04 -0400

    Update shared c# objects

--- Shared/Playlist.cs
+++ Shared/Playlist.cs
@@ -6,13 +6,10 @@ using System.Threading.Tasks;
 
 namespace music_manager_starter.Shared
 {
-    public sealed class Song
-    {
+    public sealed class Playlist
+{
         public Guid Id { get; set; }
-        public string Title { get; set; }
-        public string Artist { get; set; }
-        public string Album { get; set; }
-        public string Genre { get; set; }
+        public required string Name { get; set; }
         public List<PlaylistSongJoin> PlaylistSongJoins { get; set; }
     }
 }
---
+++ Shared/PlaylistSongJoin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace music_manager_starter.Shared
+{
+    public class PlaylistSongJoin
+    {
+        public Guid PlaylistId { get; set; } // Foreign key for playlist
+        public Playlist Playlist { get; set; } // Get and set the playlist
+
+        public Guid SongId { get; set; } // Foreign key for song
+        public Song Song { get; set; } // Get and set the song
+    }
+
+}
