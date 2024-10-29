21ba3196bed8d856e58edcbb7d49479709901810
Ryan Deisler <ryandeisler@mail.adelphi.edu>
2024-10-28 21:33:43 -0400

    Add a playlist model

---
+++ music-manager-start.Data/Models/Playlist.cs
@@ -0,0 +1,16 @@
+﻿using music_manager_starter.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace music_manager_start.Data.Models
+{
+    public sealed class Playlist
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required List<Song> Songs { get; set; }
+    }
+}
