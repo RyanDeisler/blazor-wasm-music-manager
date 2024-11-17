aeccb6bf2cc042271ccefb8c4ede73832342f979
Ryan Deisler <ryandeisler@mail.adelphi.edu>
2024-11-17 11:15:16 -0500

    Migrate join entity

---
+++ Data/Migrations/20241117161238_RestoreJoinEntity.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace music_manager_starter.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RestoreJoinEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Playlists",
+                keyColumn: "Id",
+                keyValue: new Guid("c810c3ae-6baa-4eec-babe-748c6457bbcc"));
+
+            migrationBuilder.DropColumn(
+                name: "Songs",
+                table: "Playlists");
+
+            migrationBuilder.CreateTable(
+                name: "PlaylistSongJoins",
+                columns: table => new
+                {
+                    PlaylistId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    SongId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlaylistSongJoins", x => new { x.PlaylistId, x.SongId });
+                    table.ForeignKey(
+                        name: "FK_PlaylistSongJoins_Playlists_PlaylistId",
+                        column: x => x.PlaylistId,
+                        principalTable: "Playlists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PlaylistSongJoins_Songs_SongId",
+                        column: x => x.SongId,
+                        principalTable: "Songs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlaylistSongJoins_SongId",
+                table: "PlaylistSongJoins",
+                column: "SongId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PlaylistSongJoins");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Songs",
+                table: "Playlists",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.InsertData(
+                table: "Playlists",
+                columns: new[] { "Id", "Name", "Songs" },
+                values: new object[] { new Guid("c810c3ae-6baa-4eec-babe-748c6457bbcc"), "Default playlist", "[\"Viva la Vida\",\"Yellow\",\"Shiver\"]" });
+        }
+    }
+}
